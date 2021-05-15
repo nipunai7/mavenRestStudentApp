7104dbec2edf99f4ef9e6923deede0c25856f3ee
Nipuna Muansinghe <mnipunai7@gmail.com>
2021-05-15 23:41:16 +0530

    Update StuClassController.java

--- src/main/java/com/example/studentapp/StuClassController.java
+++ src/main/java/com/example/studentapp/StuClassController.java
@@ -5,8 +5,26 @@ public class StuClassController {
     StuRepo repository = new InMemoryStuRepoImpl();
 
     @PostMapping("/student")
-    public void createStudent(@Request Student student){
+    public void createStudent(@RequestBody Student student){
         repository.save(student);
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudent(@PathVariable String id){
+        return repository.get(id);
+    }
+
+    @DeleteMapping("/student/{id}")
+    public String deleteStudent(@PathVariable String id){
+        repository.delete(id);
+        return repository.get(id).getName() +" is deleted successfully";
+    }
+
+    @PostMapping("/student/update")
+    public String updateStudent(@RequestBody Student student){
+        Student updatedStudent=repository.update(student);
+
+        return "Student details updated " +student + "to :" + updatedStudent;
     }
 
 }
